edc545ceb7abbec3d22633365f48492920393196
Kyle Herstad <herstky@gmail.com>
2018-02-07 07:48:29 -0800

    cleaned up stage_flow function. added ww flow calculation.

--- hydrocyclone_model.py
+++ hydrocyclone_model.py
@@ -1,3 +1,8 @@
+# To do:
+# implement whitewater (pull?) flow
+# implement troubleshoot mode with incremental consistency adjustment
+# include mass flows
+
 import math
 import numpy as np
 
@@ -24,28 +29,46 @@ F_flow = [0] * number_of_stages
 A_flow = [0] * number_of_stages
 R_flow = [0] * number_of_stages
 
+WW_cons = .0002
+WW_flow = [0] * (number_of_stages - 1) # WW_flow dilutes rejects of corresponding stage. no dilution on final stage
+
 flow_factor = math.sqrt(rF / rPD)
 
 #rounded_feed_flow = round(feed_flow(0), 2)
 #print('\nFeed flow =', rounded_feed_flow, 'gpm\n')
 
-#calculate all flowrates in stage i
+#calculate all flowrates in stage i. returns a matrix where index 0 corresponds to accepts and 1 to rejects
 def stage_flow(i):
 	actual_PD = F_pres[i] - A_pres[i]
 	F_flow[i] = number_of_cleaners[i] * (math.sqrt(actual_PD)  * flow_factor) ** 2
 	A = np.array([[1, 1], [A_cons[i], R_cons[i]]])
 	B = np.array([F_flow[i], F_cons[i] * F_flow[i]])
 	X = np.linalg.solve(A, B)
+	A_flow[i] = X[0]
+	R_flow[i] = X[1]
 	return(X)
   
-# run flow calculation for each stage
+# run flow calculation for each stage. maps stage_flow calculations to corresponding lists
 for i in range(0, number_of_stages):
-	A_flow[i] = stage_flow(i)[0]
-	R_flow[i] = stage_flow(i)[1]
+	stage_flow(i)
   
 print(F_flow)
 print(A_flow)
 print(R_flow)
+
+# whitewater: reject flow and consistencies are known. whitewater consisency will be given. whitewater flow calculated from reject consistency and feed consistency to following stage
+# example: reject mass fraction = 0.03, reject flow = 19 gpm, next stage feed mass fraction = .012, feed flow = 170 gpm, whitewater mass fraction = 0.0002. 
+
+# R1_flow + WW_flow = F2_flow 
+# R1_flow * R1_cons + WW_flow * WW_cons = F2_flow * F2_cons <-- this overdefines the system(?)
+# WW_flow = (F2_flow * F2_cons - R1_flow * R1_cons) / WW_cons 
+
+
+for i in range(0, number_of_stages - 1):
+	WW_flow[i] = F_flow[i + 1] - R_flow[i]
+
+print(WW_flow)
+
 
 
 
