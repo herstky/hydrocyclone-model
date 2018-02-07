994470f64dd89e99736c5bfea014d3155baf390a
Kyle Herstad <herstky@gmail.com>
2018-02-06 20:05:30 -0800

    updated code

--- hydrocyclone_model.py
+++ hydrocyclone_model.py
@@ -1,49 +1,51 @@
 import math
-import tkinter
+import numpy as np
 
-#need to update python
+np.set_printoptions(suppress=True)
 
 rF = 163.0 #gpm
 rPD = 21.0 #psid
-number_of_cleaners = 1
-# request user input for cleaner style or manual reference data. use CLP 700 as default
 
-consistencies = dict()
-pressures = dict()
+number_of_cleaners = [1, 1, 1]
 
-# consistencies are listed as %
-consistencies['F1'] = 1.00
-consistencies['A1'] = 0.83
-consistencies['R1'] = 2.32
-consistencies['F2'] = 0.90
-consistencies['A2'] = 0.80
-consistencies['R2'] = 2.42
-consistencies['F3'] = 1.25
-consistencies['A3'] = 0.97
-consistencies['R3'] = 3.33
+# consistencies are listed as mass fractions
+F_cons = [.01, .009, .0125]
+A_cons = [.0083, .008, .0097]
+R_cons = [.0232, .0242, .0333]
 
 # pressures are in psi
-pressures['F1'] = 34.5
-pressures['A1'] = 14.0
-pressures['R1'] = 12.8
-pressures['F2'] = 36.7
-pressures['A2'] = 11.1
-pressures['R2'] = 13.5
-pressures['F3'] = 38.0
-pressures['A3'] = 16.3
-pressures['R3'] = 10.4
+F_pres = [34.5, 36.7, 38]
+A_pres = [12, 11.1, 16.3]
+R_pres = [12.8, 11.1, 16.3]
 
 # aFF = ((aPD)^(1/2)*((rFF)^(1/2)/(rPD)^(1/2)))^(2)
 
-# 1/2 is evaluated as an integer. need to update to python 3.x to get expected results
 flow_factor = math.sqrt(rF / rPD)
 
-def feed_flow(actual_feed_pressure, actual_accept_pressure):
-  actual_PD = actual_feed_pressure - actual_accept_pressure
-  flow = number_of_cleaners * (math.sqrt(actual_PD)  * flow_factor) ** 2
+#calculate the feed flow of stage i
+def feed_flow(i):
+  actual_PD = F_pres[i] - A_pres[i]
+  flow = number_of_cleaners[i] * (math.sqrt(actual_PD)  * flow_factor) ** 2
   return(flow)
 
-rounded_feed_flow = round(feed_flow(pressures['F1'], pressures['A1']), 2)
-print('\nFeed flow =', rounded_feed_flow, 'gpm\n')
+#rounded_feed_flow = round(feed_flow(0), 2)
+#print('\nFeed flow =', rounded_feed_flow, 'gpm\n')
+
+#calculate the accept and reject flows of stage i
+def stage_flow(i):
+  A = np.array([[1, 1], [A_cons[i], R_cons[i]]])
+  B = np.array([feed_flow(i), F_cons[i] * feed_flow(i)])
+  X = np.linalg.solve(A, B)
+  return(X)
+  
+# flowrates are in gpm
+F_flow = [feed_flow(0), feed_flow(1), feed_flow(2)]
+A_flow = [stage_flow(0)[0], stage_flow(1)[0], stage_flow(2)[0]]
+R_flow = [stage_flow(0)[1], stage_flow(1)[1], stage_flow(2)[1]]
+  
+print(F_flow)
+print(A_flow)
+print(R_flow)
+
 
 
