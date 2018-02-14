32b84d30bf4f47ca170d5aa7c787f9961f9133ca
Kyle Herstad <herstky@gmail.com>
2018-02-13 22:07:09 -0800

    moved all functions to class methods and made most variables instance variables. programming seems to be working

--- core.py
+++ core.py
@@ -11,84 +11,115 @@ from PyQt5.QtGui import *
 np.set_printoptions(suppress=True) #converts numbers from scientific to standard notation
 
 #consistencies are in %
-field_cons = {}
-consistencies = {}
 
-#pressures are in psi
-field_pres = {}
-pressures = {}
 
-number_of_stages = 2
 
-#reference cleaner values
-rF = 163.0 #gpm
-rPD = 21.0 #psid
 
-number_of_cleaners = [1] * number_of_stages #update this 
 
-# flow rates are in gpm. assign placeholder values
-F_flow = [0] * number_of_stages
-A_flow = [0] * number_of_stages
-R_flow = [0] * number_of_stages
 
-WW_cons = .0002
-WW_flow = [0] * (number_of_stages - 1) # WW_flow dilutes rejects of corresponding stage. no dilution on final stage
 
-flow_factor = math.sqrt(rF / rPD)
-
-def foo():
-    print("bar")
-
-def stage_flow(i):
-    #!!THIS DOESNT TAKE WW INTO ACCOUNT!!
-    #print(consistencies['{}F'.format(i + 1)])
-    actual_PD = pressures['{}F'.format(i + 1)] - pressures['{}A'.format(i + 1)]
-    #print(actual_PD)
-    F_flow[i] = number_of_cleaners[i] * (math.sqrt(actual_PD) * flow_factor) ** 2
-    A = np.array([[1, 1], [consistencies['{}A'.format(i + 1)], consistencies['{}R'.format(i + 1)]]])
-    B = np.array([F_flow[i], consistencies['{}F'.format(i + 1)] * F_flow[i]])
-    #print(A, B)
-    X = np.linalg.solve(A, B)
-    A_flow[i] = X[0]
-    R_flow[i] = X[1]
-
-    if i < number_of_stages - 1:
-        WW_flow[i] = F_flow[i + 1] - R_flow[i]
-    
-    return()
-
-def calculate_flow():
-    for i in range(0, number_of_stages):
-        stage_flow(i)
-        print(F_flow[i])
-        print(A_flow[i])
-        print(R_flow[i])
-        if i < number_of_stages - 1:
-            print(WW_flow[i])
 
 class hydrocyclones(QWidget):
 
     def __init__(self):
         super().__init__()
+        
+        self.number_of_stages = self.get_number_of_stages()
+        print(self.number_of_stages)
+        
+        self.field_cons = {}
+        self.consistencies = {}
+
+        #pressures are in psi
+        self.field_pres = {}
+        self.pressures = {}
+
+        #reference cleaner values
+        self.rF = 163.0 #gpm
+        self.rPD = 21.0 #psid
+
+        self.WW_cons = .0002                     
+
+        # flow rates are in gpm. assign placeholder values
+        self.F_flow = [0] * self.number_of_stages
+        self.A_flow = [0] * self.number_of_stages
+        self.R_flow = [0] * self.number_of_stages
+
+        self.number_of_cleaners = [1] * self.number_of_stages #update this 
+
+        self.WW_flow = [0] * (self.number_of_stages - 1) # WW_flow dilutes rejects of corresponding stage. no dilution on final stage
+
+        self.flow_factor = math.sqrt(self.rF / self.rPD)
 
         self.initUI()
     
-    @staticmethod  
-    def calculate():
-    
+    #should this be a class method?
+
+
+    def get_number_of_stages(self):
+
+        stages = ("1", "2", "3", "4", "5", "6", "7")
+        self.num, ok = QInputDialog.getItem(self, "Setup", "Number of stages:", stages, 0, False)
+        if ok and self.num:
+            return(int(self.num))
+        else:
+            app.exec()
+
+
+    #this should probably be a class method
+    # @staticmethod  
+    def calculate(self):
+        print(self)
+        print(self.number_of_stages)
         try:
-            for i in range(0, number_of_stages):
+            for i in range(0, self.number_of_stages):
                 #print(float(field_cons['{}F'.format(i + 1)].text()) + 1)
-                consistencies.update({'{}F'.format(i + 1): float(field_cons['{}F'.format(i + 1)].text()), 
-                    '{}A'.format(i + 1): float(field_cons['{}A'.format(i + 1)].text()), '{}R'.format(i + 1): float(field_cons['{}R'.format(i + 1)].text())})
-                pressures.update({'{}F'.format(i + 1): float(field_pres['{}F'.format(i + 1)].text()), 
-                    '{}A'.format(i + 1): float(field_pres['{}A'.format(i + 1)].text()), '{}R'.format(i + 1): float(field_pres['{}R'.format(i + 1)].text())})
+                self.consistencies.update({'{}F'.format(i + 1): float(self.field_cons['{}F'.format(i + 1)].text()), 
+                    '{}A'.format(i + 1): float(self.field_cons['{}A'.format(i + 1)].text()), '{}R'.format(i + 1): float(self.field_cons['{}R'.format(i + 1)].text())})
+                self.pressures.update({'{}F'.format(i + 1): float(self.field_pres['{}F'.format(i + 1)].text()), 
+                    '{}A'.format(i + 1): float(self.field_pres['{}A'.format(i + 1)].text()), '{}R'.format(i + 1): float(self.field_pres['{}R'.format(i + 1)].text())})
                 #print(pressures['{}A'.format(i + 1)])
         except(ValueError): 
             print("Please enter a value in each field")
 
 
+
+    def foo():
+        print("bar")
+
+    def stage_flow(self, i):
+        #!!THIS DOESNT TAKE WW OR ACCEPTS RECIRC INTO ACCOUNT!!
+        #print(consistencies['{}F'.format(i + 1)])
+        self.actual_PD = self.pressures['{}F'.format(i + 1)] - self.pressures['{}A'.format(i + 1)]
+        #print(actual_PD)
+        self.F_flow[i] = self.number_of_cleaners[i] * (math.sqrt(self.actual_PD) * self.flow_factor) ** 2
+        self.A = np.array([[1, 1], [self.consistencies['{}A'.format(i + 1)], self.consistencies['{}R'.format(i + 1)]]])
+        self.B = np.array([self.F_flow[i], self.consistencies['{}F'.format(i + 1)] * self.F_flow[i]])
+        #print(A, B)
+        self.X = np.linalg.solve(self.A, self.B)
+        self.A_flow[i] = self.X[0]
+        self.R_flow[i] = self.X[1]
+
+        if i < self.number_of_stages - 1:
+            self.WW_flow[i] = self.F_flow[i + 1] - self.R_flow[i]
+        
+        return()
+
+    def calculate_flow(self):
+        for i in range(0, self.number_of_stages):
+            self.stage_flow(i)
+            print(self.F_flow[i])
+            print(self.A_flow[i])
+            print(self.R_flow[i])
+            if i < self.number_of_stages - 1:
+                print(self.WW_flow[i])
+
+ 
+
+
     def initUI(self):
+
+        
 
         sys_info_title = QLabel('System Info:')
         cons_title = QLabel('Consistencies (%):')
@@ -100,12 +131,12 @@ class hydrocyclones(QWidget):
         cons_grid = QGridLayout()
         pres_grid = QGridLayout()
 
-
-        for i in range(0, number_of_stages):
-            field_cons.update({'{}F'.format(i + 1): QLineEdit(), '{}A'.format(i + 1): QLineEdit(), '{}R'.format(i + 1): QLineEdit()})
-            cons_grid.addWidget(field_cons['{}F'.format(i + 1)], i, 1)
-            cons_grid.addWidget(field_cons['{}A'.format(i + 1)], i, 3)
-            cons_grid.addWidget(field_cons['{}R'.format(i + 1)], i, 5)
+        print(self.number_of_stages)
+        for i in range(0, self.number_of_stages):
+            self.field_cons.update({'{}F'.format(i + 1): QLineEdit(), '{}A'.format(i + 1): QLineEdit(), '{}R'.format(i + 1): QLineEdit()}) #map fields to a dictionary
+            cons_grid.addWidget(self.field_cons['{}F'.format(i + 1)], i, 1)
+            cons_grid.addWidget(self.field_cons['{}A'.format(i + 1)], i, 3)
+            cons_grid.addWidget(self.field_cons['{}R'.format(i + 1)], i, 5)
             
             cons_feed_label = QLabel('{}F ='.format(i + 1))
             cons_accepts_label = QLabel('{}A ='.format(i + 1))
@@ -114,10 +145,10 @@ class hydrocyclones(QWidget):
             cons_grid.addWidget(cons_accepts_label, i, 2)
             cons_grid.addWidget(cons_rejects_label, i, 4)
 
-            field_pres.update({'{}F'.format(i + 1): QLineEdit(), '{}A'.format(i + 1): QLineEdit(), '{}R'.format(i + 1): QLineEdit()})
-            pres_grid.addWidget(field_pres['{}F'.format(i + 1)], i, 1)
-            pres_grid.addWidget(field_pres['{}A'.format(i + 1)], i, 3)
-            pres_grid.addWidget(field_pres['{}R'.format(i + 1)], i, 5)            
+            self.field_pres.update({'{}F'.format(i + 1): QLineEdit(), '{}A'.format(i + 1): QLineEdit(), '{}R'.format(i + 1): QLineEdit()})
+            pres_grid.addWidget(self.field_pres['{}F'.format(i + 1)], i, 1)
+            pres_grid.addWidget(self.field_pres['{}A'.format(i + 1)], i, 3)
+            pres_grid.addWidget(self.field_pres['{}R'.format(i + 1)], i, 5)            
 
             pres_feed_label = QLabel('{}F ='.format(i + 1))
             pres_accepts_label = QLabel('{}A ='.format(i + 1))
@@ -126,6 +157,8 @@ class hydrocyclones(QWidget):
             pres_grid.addWidget(pres_accepts_label, i, 2)
             pres_grid.addWidget(pres_rejects_label, i, 4)
         
+        cons_grid.addWidget(QLabel('WW ='), self.number_of_stages, 0)
+        cons_grid.addWidget(QLineEdit(), self.number_of_stages, 1)
 
         data_hbox = QHBoxLayout() #outermost box
         sys_info_vbox = QVBoxLayout()
@@ -134,14 +167,16 @@ class hydrocyclones(QWidget):
         pres_vbox = QVBoxLayout()
 
         data_hbox.addLayout(sys_info_vbox)
-        data_hbox.addLayout(cons_vbox)
-        data_hbox.addLayout(pres_vbox)
-        data_hbox.addStretch(1) #not sure what his does. need to push boxes left
+ 
 
         sys_info_vbox.addWidget(sys_info_title)
         sys_info_vbox.addLayout(sys_grid)
         sys_info_vbox.addStretch(1)
         sys_info_vbox.setContentsMargins(0, 0, 20, 0)
+
+        data_hbox.addLayout(cons_vbox)
+        data_hbox.addLayout(pres_vbox)
+        data_hbox.addStretch(1) #not sure what his does. need to push boxes left
 
         cons_vbox.addWidget(cons_title) #vertical box containing consistency table
         cons_vbox.addLayout(cons_grid)
@@ -161,7 +196,7 @@ class hydrocyclones(QWidget):
 
 
         calculate_button.clicked.connect(self.calculate)
-        calculate_button.clicked.connect(calculate_flow)
+        calculate_button.clicked.connect(self.calculate_flow)
 
         
         self.setGeometry(300, 300, 300, 150)
